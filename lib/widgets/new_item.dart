3756aa88f453632d0c9b04eac31d579277d61019
vohoangloc123 <you@example.com>
2024-09-24 12:41:32 +0700

    Add validation logic

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -26,7 +26,13 @@ class _NewItemState extends State<NewItem> {
                   labelText: 'Item name',
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters';
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -38,6 +44,16 @@ class _NewItemState extends State<NewItem> {
                         labelText: 'Quantity',
                       ),
                       initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a number greater than 0';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
