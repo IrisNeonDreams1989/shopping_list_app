26dc2f3e26e9df1c26666ac5880e0853c87dad8e
vohoangloc123 <you@example.com>
2024-09-30 13:06:58 +0700

    Use the FutureBuilder Widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,59 +15,53 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  late Future<List<GroceryItem>> _loadedItems;
   var _error = '';
   var _isLoading = true;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
+  Future<List<GroceryItem>> _loadItems() async {
     final url = Uri.https(
       'shoppinglistsapp-7e589-default-rtdb.asia-southeast1.firebasedatabase.app',
       '/shopping-list.json',
     );
-    try {
-      final response = await http.get(url);
-      if (response.statusCode >= 400) {
-        // Xử lý lỗi nếu xảy ra
-        setState(() {
-          _error = 'Failed to fetch data! Please check your connection.';
-        });
-      }
-      final Map<String, dynamic> listData = json.decode(response.body);
-      final List<GroceryItem> _loadedItem = [];
-      for (final item in listData.entries) {
-        final category = categories.entries
-            .firstWhere(
-                (element) => element.value.title == item.value['category'])
-            .value;
-        _loadedItem.add(
-          GroceryItem(
-            id: item.key,
-            name: item.value['name'],
-            quantity: item.value['quantity'],
-            category: category,
-          ),
-        );
-      }
+
+    final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      throw Exception('Failed to fetch grocery items. Please try again later');
+    }
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItem = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (element) => element.value.title == item.value['category'])
+          .value;
+      _loadedItem.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = _loadedItem;
+      _isLoading = false;
+    });
+    if (response.body == 'null') {
       setState(() {
-        _groceryItems = _loadedItem;
         _isLoading = false;
       });
-      if (response.body == 'null') {
-        setState(() {
-          _isLoading = false;
-        });
-        return;
-      }
-    } catch (error) {
-      setState(() {
-        _error = 'Something went wrong! $error';
-      });
+      return [];
     }
+    return _loadedItem;
   }
 
   void _addItem(BuildContext context) async {
@@ -108,36 +102,7 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     Widget content = Center(child: Text('No items yet!'));
-    if (_isLoading) {
-      content = const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => Dismissible(
-          onDismissed: (direction) {
-            _removeItem(_groceryItems[index]);
-          },
-          key: ValueKey(_groceryItems[index].id),
-          child: ListTile(
-            title: Text(_groceryItems[index].name),
-            leading: Container(
-              width: 50,
-              height: 50,
-              color: _groceryItems[index].category.color,
-            ),
-            trailing: Text('${_groceryItems[index].quantity}'),
-          ),
-        ),
-      );
-    }
-    if (_error.isNotEmpty) {
-      content = Center(
-        child: Text(_error),
-      );
-    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your groceries'),
@@ -148,7 +113,46 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: content,
+      //việc dùng Future là để xử lý các thao tác bất đồng bộ,
+      //giữ cho ứng dụng phản hồi nhanh, và không gây chậm trễ cho giao diện người dùng
+      body: FutureBuilder(
+        future: _loadedItems,
+        builder: (content, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+          if (snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text('No items yet!'),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(snapshot.data![index]);
+              },
+              key: ValueKey(snapshot.data![index].id),
+              child: ListTile(
+                title: Text(snapshot.data![index].name),
+                leading: Container(
+                  width: 50,
+                  height: 50,
+                  color: snapshot.data![index].category.color,
+                ),
+                trailing: Text('${snapshot.data![index].quantity}'),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
